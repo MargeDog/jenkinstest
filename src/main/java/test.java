b4b86711ce0cdd934fa7021e37c9dc106dfb4aeb
MH <wangyongqiang@MHdeMacBook-Pro.local>
2019-11-19 15:18:25 +0800

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,13 @@
+
+
+/**
+ * @description:
+ * @author: MH
+ * @createDate: 2019/11/19
+ * @version: 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test success");
+    }
+}
